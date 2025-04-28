ef20dbfb5c33dd9f5aef35e31e47ec129a81dd5c
Techify Bots <TechifyBots@gmail.com>
2025-04-28 19:25:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 
 API_ID = os.getenv("API_ID", "")
 API_HASH = os.getenv("API_HASH", "")
@@ -10,9 +11,9 @@ LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", ""))
 DB_URI = os.getenv("DB_URI", "")
 DB_NAME = os.getenv("DB_NAME", "")
 
-IS_FSUB = bool(os.environ.get("FSUB", False)) # Set "True" For Force Subscribe Enable
-AUTH_CHANNELS = os.environ.get("AUTH_CHANNEL", "") # Add Multiple Channels iD By Space
-AUTH_CHANNELS = [int(channel_id) for channel_id in AUTH_CHANNELS.split(",")] # DONT TOUCH
+IS_FSUB = bool(os.environ.get("FSUB", False)) # Set "True" For Enable Force Subscribe
+AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-100******** -100*********").split())) # Add Multiple channel id
+
 
 EMOJIS = [
     "👍", "🔥", "🥰", "👏", "😁", "🤔", "🤯", "😱", "🤬", "😢", 
